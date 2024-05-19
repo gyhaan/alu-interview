b8148d00d0b8146bf35e0c4616114e2edf3cde79
gyhaan <g.yhaan@alustudent.com>
2024-05-19 08:09:24 +0200

    Refactor minOperations function to comply with PEP 8

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,16 @@
+def min_operations(n):
+    """"This function will find the minimum number of operations needed"""
+
+    if n <= 1:
+        return 0
+
+    operations = 0
+    factor = 2
+
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+
+    return operations
